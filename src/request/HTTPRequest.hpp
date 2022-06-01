7f49586b80781cec922015f7e08b1a13da992993
rsudo <badbad7351@gmail.com>
2022-06-01 22:25:27 +0900

    :shower: parserで実装したrequestのメソッドを削除

--- src/request/HTTPRequest.hpp
+++ src/request/HTTPRequest.hpp
@@ -8,9 +8,8 @@
 class HTTPRequest {
 public:
     HTTPRequest();
-    HTTPRequest(const std::string& row);
-    HTTPRequest(std::string method, std::string uri);
     ~HTTPRequest();
+
     const std::string&                        GetMethod() const;
     std::string                               GetRequestTarget() const;
     const std::string&                        GetVersion() const;
@@ -24,9 +23,6 @@ public:
     void SetStatus(const int status);
     void SetHeader(const std::string key, const std::string value);
 
-    // 対応するメソッド一覧。仮でここで宣言
-    static std::set<std::string> methods;
-
     // 改行コード
     static const std::string            crlf;
     static const std::string::size_type crlf_size;
@@ -38,7 +34,6 @@ public:
     static const int status_version_not_supported = 505;
 
 private:
-    std::string                        request_message_;
     std::string                        method_;
     std::string                        request_target_;
     std::string                        HTTPVersion_;
@@ -46,30 +41,6 @@ private:
     const std::string                  row_;
     std::map<std::string, std::string> headers_;
     int                                status_;
-
-    void parse();
-    void parseFirstline(const std::string& line);
-    void parseHeaderLines(std::string& str);
-    void parseHeaderLine(const std::string& line);
-    void parseBody(std::string& body);
-
-    void throwErrorBadrequest(const std::string err_message);
-    void throwErrorMethodNotAllowed(const std::string err_message);
-    void throwErrorVersionNotSupported(const std::string err_message);
-
-    void validateMethod(const std::string& method);
-    void validateRequestTarget(const std::string& request_target);
-    void validateHTTPVersion(const std::string& version);
-    void validateToken(const std::string& token);
-    void validateBody(const std::string& body);
-    void validateVersionNotSuppoted();
-    void validateMethodNotAllowed();
-
-    std::string            trimSpace(const std::string& string,
-                                     const std::string  trim_char_set) const;
-    std::string::size_type mustFindCRLF(const std::string& str);
-    bool                   isdigit(const std::string& str) const;
-    bool                   hostExists() const;
 };
 
 #endif
