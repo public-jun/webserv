1c9fdeeb46ed9c703c08c4e34a00ce8b66135e34
rsudo <badbad7351@gmail.com>
2022-06-01 18:06:46 +0900

    :+1: HTTPRequestのテストをHTTPParserに移植

--- test/httpparser.cpp
+++ test/httpparser.cpp
@@ -118,10 +118,157 @@ TEST(HTTPParser, PhaseHeaderLine) {
 // === Badrequest CASE ===
 
 TEST(HTTPParser, EmptyMethod) {
-    string      message("/ HTTP/1.1\r\nHOST:localhost\r\n\r\n");
+    string      message("/ HTTP/1.1\r\nHost:localhost\r\n\r\n");
     HTTPRequest req;
     HTTPParser  parser(req);
     parser.ParsePart(message);
 
     EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, LowercaseMethod) {
+    string      message("get / HTTP/1.1\r\nHost: localhost\r\n\r\n");
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionNotExistName) {
+    string      message = "GET / /1.1\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionLowerCase) {
+    string      message = "GET / http/1.1\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionMultipleDot) {
+    string      message = "GET / HTTP/1.1.1\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionEndWithDot) {
+    string      message = "GET / HTTP/1.\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionStartWithDot) {
+    string      message = "GET / HTTP/.1\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionOnlyName) {
+    string      message = "GET / HTTP/\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionNotExistSlash) {
+    string      message = "GET / HTTP1.1\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionNotExistdot) {
+    string      message = "GET / HTTP/1\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, NoHost) {
+    string      message = "GET / HTTP/1.1\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, ValueContainCR) {
+    string      message = "GET / HTTP/1.1\r\nHost: local\rhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, ValueContainLF) {
+    string      message = "GET / HTTP/1.1\r\nHost: local\nhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, KeyIncludeSpace) {
+    string      message = "GET / HTTP/1.1\r\nHost : localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+TEST(HTTPParser, EmptyKey) {
+    string      message = "GET / HTTP/1.1\r\nHost: localhost\r\n:hoge\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(400, req.GetStatus());
+}
+
+// =======================
+// == OTHER STATUS CASE ==
+
+TEST(HTTPParser, UnsupportedMethod) {
+    string      message = "HOGE / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(405, req.GetStatus());
+}
+
+TEST(HTTPParser, VersionNotSupported) {
+    string      message = "GET / HTTP/3.0\r\nHost: localhost\r\n\r\n";
+    HTTPRequest req;
+    HTTPParser  parser(req);
+    parser.ParsePart(message);
+
+    EXPECT_EQ(505, req.GetStatus());
 }
