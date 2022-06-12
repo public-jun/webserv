6d3f35d4906e0e5655b70bcbf46ff65a96227bde
rsudo <badbad7351@gmail.com>
2022-06-12 15:39:29 +0900

    :up: DefaultErrorPageの定義の書き方変更

--- src/response/DefaultErrorPage.hpp
+++ src/response/DefaultErrorPage.hpp
@@ -5,30 +5,31 @@
 namespace DefaultErrorPage {
 
 const std::string bad_request =
-    std::string("<html>\r\n") +
-    std::string("<head><title>400 Bad Request</title></head>\r\n") +
-    std::string("<body>\r\n") +
-    std::string("<center><h1>400 Bad Request</h1></center>\r\n") +
-    std::string("<hr><center>webserv</center>\r\n") +
-    std::string("</body>\r\n") + std::string("</html>\r\n");
+    "<html>\r\n"
+    "<head><title>400 Bad Request</title></head>\r\n"
+    "<body>\r\n"
+    "<center><h1>400 Bad Request</h1></center>\r\n"
+    "<hr><center>webserv</center>\r\n"
+    "</body>\r\n"
+    "</html>\r\n";
 
 const std::string method_not_allowed =
-    std::string("<html>\r\n") +
-    std::string("<head><title>405 Not Allowed</title></head>\r\n") +
-    std::string("<body>\r\n") +
-    std::string("<center><h1>405 Not Allowed</h1></center>\r\n") +
-    std::string("<hr><center>webserv</center>\r\n") +
-    std::string("</body>\r\n") + std::string("</html>\r\n");
+    "<html>\r\n"
+    "<head><title>405 Not Allowed</title></head>\r\n"
+    "<body>\r\n"
+    "<center><h1>405 Not Allowed</h1></center>\r\n"
+    "<hr><center>webserv</center>\r\n"
+    "</body>\r\n"
+    "</html>\r\n";
 
 const std::string version_not_suppoted =
-    std::string("<html>\r\n") +
-    std::string(
-        "<head><title>505 HTTP Version Not Supported</title></head>\r\n") +
-    std::string("<body>\r\n") +
-    std::string(
-        "<center><h1>505 HTTP Version Not Supported</h1></center>\r\n") +
-    std::string("<hr><center>webserv</center>\r\n") +
-    std::string("</body>\r\n") + std::string("</html>\r\n");
+    "<html>\r\n"
+    "<head><title>505 HTTP Version Not Supported</title></head>\r\n"
+    "<body>\r\n"
+    "<center><h1>505 HTTP Version Not Supported</h1></center>\r\n"
+    "<hr><center>webserv</center>\r\n"
+    "</body>\r\n"
+    "</html>\r\n";
 } // namespace DefaultErrorPage
 
 #endif // DEFAULT_ERROR_PAGE_HPP
