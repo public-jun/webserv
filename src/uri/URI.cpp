bf6ca11458e4e39a3f4fcade9bcc9a612e2174c9
public-jun <generalpublicjunjun@gmail.com>
2022-06-13 15:35:58 +0900

    :+1: URIクラスにsplitTargetメソッド追加
    raw_target_を'?'で分割するメソッドを追加
    
    About: #73

--- src/uri/URI.cpp
+++ src/uri/URI.cpp
@@ -4,7 +4,24 @@
 
 #include "ServerConfig.hpp"
 
-URI::URI(const ServerConfig& server_config, const std::string& target)
-    : server_config_(server_config), target_(target) {}
+URI::URI(const ServerConfig& server_config, const std::string target)
+    : server_config_(server_config), raw_target_(target) {
+    // ? で splitする
+    splitTarget();
+}
 
 URI::~URI() {}
+
+// raw_target を <raw_path_> '?' <query_> に分割する
+void URI::splitTarget() {
+    // ? があるかどうか調べる
+    std::string::size_type pos = raw_target_.find("?");
+
+    // <raw_path_> '?' <query_> に分割
+    raw_path_ = std::string(raw_target_, 0, pos);
+    if (pos == std::string::npos) {
+        return;
+    }
+
+    query_ = raw_target_.substr(pos + 1);
+}
--- src/uri/URI.hpp
+++ src/uri/URI.hpp
@@ -5,18 +5,28 @@
 
 #include "ServerConfig.hpp"
 
-class URI
-{
+class URI {
 public:
-    URI(const ServerConfig& server_config, const std::string& target);
+    URI(const ServerConfig& server_config, const std::string target);
+
     ~URI();
+
+    const ServerConfig& GetServerConfig() const { return server_config_; }
+    const std::string   GetRawTarget() const { return raw_target_; }
+    const std::string   GetRawPath() const { return raw_path_; }
+    const std::string   GetQuery() const { return query_; }
 
 private:
     URI();
+    void splitTarget();
 
 private:
     const ServerConfig& server_config_;
-    const std::string& target_;
+    const std::string   raw_target_;
+
+    std::string              raw_path_;
+    std::string              query_;
+    std::vector<std::string> args;
 };
 
 #endif
