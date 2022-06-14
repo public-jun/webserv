54bb351be6fdc88ea62d4676b2a2c2f13b0d0b2a
Tatsuya Sugiyama <tatsuya.prog@gmail.com>
2022-06-14 17:17:47 +0900

    :bug: forbiddenのステータスコードを403に修正
    http://chris.beams.io/posts/git-commit/

--- src/response/HTTPStatus.hpp
+++ src/response/HTTPStatus.hpp
@@ -8,7 +8,7 @@ typedef int code;
 const code ok                   = 200;
 const code no_contenxt          = 204;
 const code bad_request          = 400;
-const code forbidden            = 400;
+const code forbidden            = 403;
 const code not_found            = 404;
 const code method_not_allowed   = 405;
 const code server_error         = 500;
