a6885b274a6c3183b826d3f357c378597121719f
Tatsuya Sugiyama <tatsuya.prog@gmail.com>
2022-06-18 20:34:40 +0900

    :+1: ファイル削除権限をその他の書き込み権限に変更
    http://chris.beams.io/posts/git-commit/

--- src/event/mode/Delete.cpp
+++ src/event/mode/Delete.cpp
@@ -14,12 +14,14 @@ void Delete::Run() {
     std::string        target_path = uri_.GetLocalPath();
     const struct stat& stat_buf    = uri_.GetStat();
 
-    if (S_ISDIR(stat_buf.st_mode) || access(target_path.c_str(), W_OK) == -1) {
+    if (S_ISDIR(stat_buf.st_mode) || (stat_buf.st_mode & S_IWOTH) != S_IWOTH) {
         status_ = status::forbidden;
         return;
     }
-    if (unlink(target_path.c_str()) == -1)
+    if (unlink(target_path.c_str()) == -1) {
         status_ = status::server_error;
+        return;
+    }
     status_ = status::no_content;
 }
 
@@ -27,7 +29,6 @@ IOEvent* Delete::RegisterNext() {
     resp_.AppendHeader("Server", "Webserv/1.0.0");
     resp_.SetStatusCode(status_);
     resp_.AppendHeader("Content-Length", "0");
-    std::cout << resp_.ConvertToStr() << std::endl;
     IOEvent* send_response = new SendResponse(stream_, resp_.ConvertToStr());
 
     send_response->Register();
